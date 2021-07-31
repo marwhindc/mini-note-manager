4f0903a1e3767e4f7caf9c392f1ea63059bfca5e
Marwhin Dean <marwhindc@gmail.com>
2021-07-31 19:35:24 +0800

    Added Add, Delete, and Copy buttons. Also addedd new dialog when adding an item

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,53 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class Controller {
+
+    @FXML
+    private BorderPane bpMain;
+    @FXML
+    private ComboBox<String> cbNotes;
+    @FXML
+    private TextField tfNewItem;
+
+
+    //Add item dialog opens when user clicks Add button beside CheckBox
+    @FXML
+    public void showAddItemDialog() {
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(bpMain.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        dialog.setHeaderText("Enter name of new Note");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("addnotedialog.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Unable to load dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        //Add entered item in dialog to ComboBox list
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+//            String newItem = tfNewItem.getText();
+//            cbNotes.getItems().add(newItem);
+        }
+    }
 }

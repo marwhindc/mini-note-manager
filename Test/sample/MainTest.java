71235bbe5ba4543e94630de696d7ecd9f37708df
Marwhin Dean <marwhindc@gmail.com>
2021-08-08 19:13:55 +0800

    Added initial unit test

---
+++ Test/sample/MainTest.java
@@ -0,0 +1,50 @@
+package sample;
+
+import static org.junit.Assert.*;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.security.Key;
+
+public class MainTest extends ApplicationTest{
+
+
+    @Override
+    public void start (Stage stage) throws Exception {
+        Parent mainNode = FXMLLoader.load(Main.class.getResource("main.fxml"));
+        stage.setScene(new Scene(mainNode));
+        stage.show();
+        stage.toFront();
+    }
+
+    @Before
+    public void setUp () throws Exception {
+    }
+
+    @After
+    public void tearDown () throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    @Test
+    public void testAddItemDialog(){
+
+        clickOn("#btAddNote");
+        clickOn("#tfNewItem");
+        write("This is a test");
+        press(KeyCode.ENTER);
+    }
+
+}
